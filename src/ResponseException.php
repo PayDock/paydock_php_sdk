bdfccb9d89e4d0325a65c17f4b9d56c13e717a70
skipper <bogdan@paydock.com>
2021-09-02 17:03:16 +0300

    P2-2725 fix exception text issue

--- src/ResponseException.php
+++ src/ResponseException.php
@@ -16,4 +16,34 @@ class ResponseException extends Exception
     public $Status;
     public $ErrorMessage;
     public $JsonResponse;
+
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+
+    /**
+     * @param mixed $Status
+     */
+    public function setCode($Status)
+    {
+        $this->code = $Status;
+    }
+
+    /**
+     * @param mixed $ErrorMessage
+     */
+    public function setMessage($ErrorMessage)
+    {
+        $this->ErrorMessage = $ErrorMessage;
+        $this->message = $ErrorMessage;
+    }
+
+    /**
+     * @param mixed $JsonResponse
+     */
+    public function setJsonResponse($JsonResponse)
+    {
+        $this->JsonResponse = $JsonResponse;
+    }
+
 }
